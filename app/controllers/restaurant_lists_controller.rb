089ce22deb624b3281d6482a6ba8ba7c374c56cd
Carlos Aguirre <craguirre952@gmail.com>
2021-07-29 21:16:30 -0400

    create index, create and show routes for restaurant_list, and respective controller methods

---
+++ app/controllers/restaurant_lists_controller.rb
@@ -0,0 +1,23 @@
+class RestaurantListsController < ApplicationController
+  def index
+    restaurant_lists = RestaurantList.all
+    render json: restaurant_lists
+  end
+
+  def show
+    restaurant_list = RestaurantList.find_by(id: params[:id])
+    render json: restaurant_list
+  end
+
+  def create
+    restaurant_list = RestaurantList.new(
+      user_list_id: params[:user_list_id],
+      restaurant_id: params[:restaurant_id]
+    )
+    if restaurant_list.save
+      render json: restaurant_list
+    else
+      render json: {error: restaurant_list.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+
+  get "/restaurant_lists" => "restaurant_lists#index"
+  get "/restaurant_lists/:id" => "restaurant_lists#show"
 end
