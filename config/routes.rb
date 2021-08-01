43350e705bc1eea5daebe9afa662b99ded4064b1
Carlos Aguirre <craguirre952@gmail.com>
2021-08-01 16:44:31 -0400

    add CORS

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 
   get "/restaurant_lists" => "restaurant_lists#index"
   get "/restaurant_lists/:id" => "restaurant_lists#show"
+  post "/restaurant_lists" => "restaurant_lists#create"
 end
