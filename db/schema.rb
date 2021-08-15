4effdf0e95099858f943fd81686a869c1b26937a
Carlos Aguirre <craguirre952@gmail.com>
2021-08-15 17:35:53 -0400

    add address to restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_210253) do
+ActiveRecord::Schema.define(version: 2021_08_15_213009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_210253) do
     t.integer "cuisine_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "users", force: :cascade do |t|
