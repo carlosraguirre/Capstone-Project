e0d58e6ee8e35aa4e1a79f33e8ef2e9825302a4c
Carlos Aguirre <craguirre952@gmail.com>
2021-11-04 18:33:38 -0400

    fix netlify issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', 'localhost:8080', 'yummylist.netlify.app'
+    origins 'example.com', 'localhost:8080', 'https://yummylist.netlify.app/'
 
     resource '*',
       headers: :any,
